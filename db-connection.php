503002d7eb2d9279395cc8df3683d831513d28ca
barraganmariano <mariano94b@gmail.com>
2017-10-27 20:11:07 -0300

    mas datos hardcodeados

--- db-connection.php
+++ db-connection.php
@@ -8,9 +8,6 @@
 
 	$database = new Model();
 	
-
-	//echo $database->escribir_sexo("AAA");
-
 	$datos_persona["nombre"] = "nombre";
 	$datos_persona["apellido"] = "apellido"; 
 	$datos_persona["id_sexo"] = 111; 
@@ -27,14 +24,30 @@
 	
 	$experiencias_laborales = array(
 		0 => Array(
-				'idioma' => "ingles",
-				'sabe_oral'=>true,
-				'sabe_escrito'=>false,
+				'empresa' => "empresa X",
+				'actividad'=> "actividad x",
+				'puesto'=> "el puesto",
+				'nivel'=> "el nivel-",
+				'pais'=> "arg",
+				'inicio_actividad'=> 2010-06-02,
+				'fin_actividad'=> 2010-06-02,
+				'area_del_puesto'=> "el area del puesto",
+				'descripcion'=> "gran trabajo",
+				'cantidad_personas_a_cargp'=> 21,
+				'nombre_persona_de_referencia'=> "carlos",
 			),
 		1 => Array(
-				'idioma'=> "español",
-				'sabe_oral'=>true,
-				'sabe_escrito'=>true,
+				'empresa' => "empresa y",
+				'actividad'=> "actividad Y",
+				'puesto'=> "el 2 puesto",
+				'nivel'=> "el 2 nivel-",
+				'pais'=> "chile wn",
+				'inicio_actividad'=> 2015-06-02,
+				'fin_actividad'=> 2015-06-02,
+				'area_del_puesto'=> "el  2area del puesto",
+				'descripcion'=> "descripcion trabajo",
+				'cantidad_personas_a_cargp'=> 0,
+				'nombre_persona_de_referencia'=> "otro carlos",
 			)
 
 		);
